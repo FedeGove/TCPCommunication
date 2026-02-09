e8f9cda315bbb47e74409c45e2498d4437e1c128
FedeGove <federico.governatori07@gmail.com>
2026-02-09 13:29:38 +0100

    prime modifiche

--- TCPCommunication/src/Client/MainClient.java
+++ TCPCommunication/src/Client/MainClient.java
@@ -1,7 +1,17 @@
 package Client;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class MainClient {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 12345);
+
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("Could not connect to server");
+        }
     }
 }
--- TCPCommunication/src/Server/MainServer.java
+++ TCPCommunication/src/Server/MainServer.java
@@ -1,6 +1,34 @@
 package Server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        int port = 12345;
+
+        try  {
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Server is listening on port " + port);
+            
+            Socket clientSocket = server.accept();
+            System.out.println("Client connected");
+
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            clientSocket.close();
+            server.close();
+
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + port);
+        }
+
+
+        
     }
 }
